17ccbd7e81d288e0eda682e8e54790acf661e7df
Satoshi Kawabata <satoshi.kawabata@nulab-inc.com>
2017-12-11 18:06:37 +0900

    Deploy

---
+++ build/default/components/app-container.js
@@ -0,0 +1,7 @@
+import { Element as PolymerElement } from '../node_modules/@polymer/polymer/polymer-element.js';
+
+export default class AppContainer extends PolymerElement {
+    static get template() {
+        return `<div>Hello</div>`;
+    }
+}
